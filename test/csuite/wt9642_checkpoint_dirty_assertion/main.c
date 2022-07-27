49545f32ae298aee25f1b9ca7691a934f7e18f00
Ravi Giri <ravi.giri@mongodb.com>
2022-07-27 14:17:10 +0000

    Updated the test to open list of cursors before updating the table

--- test/csuite/wt9642_checkpoint_dirty_assertion/main.c
+++ test/csuite/wt9642_checkpoint_dirty_assertion/main.c
@@ -40,6 +40,8 @@
 static const char table_config[] = "key_format=S,value_format=i";
 static void *thread_func_checkpoint(void *);
 
+bool done = false;
+
 /* Structures definition. */
 struct thread_data {
     WT_CONNECTION *conn;
@@ -57,20 +59,19 @@ thread_func_checkpoint(void *arg)
     WT_SESSION *session;
 
     td = (struct thread_data *)arg;
-
     testutil_check(td->conn->open_session(td->conn, NULL, NULL, &session));
 
-    // Sleep for 10 milliseconds.
-    __wt_sleep(0.01, 0);
-
-    testutil_check(session->checkpoint(session, NULL));
+    while (!done)
+    {
+        // Sleep for 10 milliseconds.
+        __wt_sleep(0.01, 0);
+        testutil_check(session->checkpoint(session, NULL));
+    }
 
     testutil_check(session->close(session, NULL));
     session = NULL;
 
-
     return (NULL);
-
 }
 
 
@@ -82,9 +83,9 @@ int
 main(int argc, char *argv[])
 {
     TEST_OPTS *opts, _opts;
-    //WT_CONNECTION *conn;
     WT_SESSION *session;
     WT_CURSOR *cursor;
+    WT_CURSOR *cursor_list[NUM_TABLES];
     struct thread_data td;
     pthread_t thread_checkpoint;
 
@@ -105,7 +106,7 @@ main(int argc, char *argv[])
 
     for (i = 0; i < NUM_TABLES; i++) {
         sprintf(uri, URI_FORMAT, i);
-        printf("Creating table : %s\n", uri);
+        //printf("Creating table : %s\n", uri);
         /* Create and populate table. Checkpoint the data after that. */
         testutil_check(session->create(session, uri, table_config));
 
@@ -119,21 +120,25 @@ main(int argc, char *argv[])
     testutil_check(session->checkpoint(session, NULL));
 
     expected_val = 0;
+
+    /* Open cursors. */
+    for (i = 0; i < NUM_TABLES; i++) {
+        sprintf(uri, URI_FORMAT, i);
+        testutil_check(session->open_cursor(session, uri, NULL, NULL, &cursor_list[i]));
+    }
     for (i = 0; i < 10; ++i) {
         printf("Doing iteration : %d\n", i);
-
         testutil_check(pthread_create(&thread_checkpoint, NULL, thread_func_checkpoint, &td));
 
         expected_val += 1;
         for (int j = 0; j < NUM_TABLES; ++j) {
-            sprintf(uri, URI_FORMAT, j);
-            testutil_check(session->open_cursor(session, uri, NULL, NULL, &cursor));
-
-            cursor->set_key(cursor, "a");
-            cursor->set_value(cursor,expected_val);
-            testutil_check(cursor->update(cursor));
-            cursor->close(cursor);
+            cursor_list[j]->set_key(cursor_list[j], "a");
+            cursor_list[j]->set_value(cursor_list[j],expected_val);
+            testutil_check(cursor_list[j]->update(cursor_list[j]));
+            //cursor->close(cursor);
         }
+
+        done = true;
 
         /* Wait for the threads to finish the work. */
         (void)pthread_join(thread_checkpoint, NULL);
@@ -155,9 +160,10 @@ main(int argc, char *argv[])
             testutil_assert(table_value == expected_val);
             cursor->close(cursor);
         }
-
-
     }
 
-
+    /* Close cursors. */
+    for (i = 0; i < NUM_TABLES; i++) {
+        cursor_list[i]->close(cursor_list[i]);
+    }
 }
