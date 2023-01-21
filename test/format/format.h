f2a3790424a8c86590e896ebcf6416df63b9e391
Mick Graham <mick.graham@mongodb.com>
2023-01-21 16:59:36 +0000

    Addition debugging and tuning down the Antithesis config

--- test/format/format.h
+++ test/format/format.h
@@ -183,6 +183,7 @@ extern struct timespec test_start_time;
         __wt_epoch(NULL, &test_time_now);                                                      \
         printf(                                                                                \
           "Lapsed %" PRIu64 " ms: %s\n", WT_TIMEDIFF_MS(test_time_now, test_start_time), msg); \
+        fflush(stdout);                                                                        \
     }
 #define LAPSED_TS_MSG_D64(msg, v)                                  \
     {                                                              \
@@ -190,6 +191,7 @@ extern struct timespec test_start_time;
         __wt_epoch(NULL, &test_time_now);                          \
         printf("Lapsed %" PRIu64 " ms: %s%" PRId64 "\n",           \
           WT_TIMEDIFF_MS(test_time_now, test_start_time), msg, v); \
+        fflush(stdout);                                            \
     }
 
 typedef struct {
